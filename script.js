1cbaa48b2f22f3645dd534aa1dfab2bb5f0d368a
zach-koors <67700062+zach-koors@users.noreply.github.com>
2021-04-06 13:59:13 -0400

    added JS but it's not working, trying to fix

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+function getHistory(){
+  return document.getElementById("history-value").innerText;
+};
+function printHistory(num){
+  document.getElementById("history-value").innerText=num;
+};
+function getOutput(){
+  return document.getElementById("output-value").innerText;
+};
+function printOutput(num){
+  document.getElementById("output-value").innerText=num;
+};
